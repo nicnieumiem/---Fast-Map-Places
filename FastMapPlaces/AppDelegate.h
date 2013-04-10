81c8391d63164222981a471596ef3bc9cc7c5a83
nicnieumiem.pl <nicnieumiem@nicnieumiempls-MacBook-Pro.local>
2013-04-10 17:23:50 +0200

    Initial Commit

---
+++ FastMapPlaces/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FastMapPlaces
+//
+//  Created by nicnieumiem.pl on 4/10/13.
+//  Copyright (c) 2013 nicnieumiem.pl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FastMapPlaces/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FastMapPlaces
+//
+//  Created by nicnieumiem.pl on 4/10/13.
+//  Copyright (c) 2013 nicnieumiem.pl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
